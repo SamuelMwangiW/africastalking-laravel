d3bca34b88eb8b60f52061226ec71222db87de8e
SamuelMwangiW <SamuelMwangiW@users.noreply.github.com>
2022-04-26 01:51:10 +0000

    Fix styling

--- tests/Datasets/airtime.php
+++ tests/Datasets/airtime.php
@@ -1,29 +1,29 @@
 <?php
 
+use function Pest\Faker\faker;
 use SamuelMwangiW\Africastalking\Enum\Currency;
 use SamuelMwangiW\Africastalking\ValueObjects\AirtimeTransaction;
-use function Pest\Faker\faker;
 
 dataset('airtime-transactions', function () {
-    yield fn() => AirtimeTransaction::make(
+    yield fn () => AirtimeTransaction::make(
         phoneNumber: '+254700072929',
         currency: Currency::KENYA,
         amount: faker()->numberBetween(100, 300)
     );
 
-    yield fn() => AirtimeTransaction::make(
+    yield fn () => AirtimeTransaction::make(
         phoneNumber: '+256783879001',
         currency: Currency::UGANDA,
         amount: faker()->numberBetween(100, 300)
     );
 
-    yield fn() => AirtimeTransaction::make(
+    yield fn () => AirtimeTransaction::make(
         phoneNumber: '+255757022731',
         currency: Currency::TANZANIA,
         amount: faker()->numberBetween(1000, 3000)
     );
 
-    yield fn() => AirtimeTransaction::make(
+    yield fn () => AirtimeTransaction::make(
         phoneNumber: '+2348160663047',
         currency: Currency::NIGERIA,
         amount: faker()->numberBetween(100, 300)
@@ -31,5 +31,5 @@ dataset('airtime-transactions', function () {
 });
 
 dataset('airtime-amount', [
-    fn() => faker()->numberBetween(100, 150),
+    fn () => faker()->numberBetween(100, 150),
 ]);
