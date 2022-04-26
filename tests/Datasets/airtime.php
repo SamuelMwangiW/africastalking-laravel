6fe4ee14f4fd30323fbf3b9264c4c7dfccf012dd
Samuel Mwangi <samuel@samuelmwangi.co.ke>
2022-04-26 04:50:03 +0300

    airtime

--- tests/Datasets/airtime.php
+++ tests/Datasets/airtime.php
@@ -1,39 +1,35 @@
 <?php
 
-use function Pest\Faker\faker;
 use SamuelMwangiW\Africastalking\Enum\Currency;
 use SamuelMwangiW\Africastalking\ValueObjects\AirtimeTransaction;
+use function Pest\Faker\faker;
 
-dataset('airtime-transactions', [
-    [
-        fn () => AirtimeTransaction::make(
-            phoneNumber: '+254720123123',
-            currency: Currency::KENYA,
-            amount: faker()->numberBetween(100, 300)
-        ),
-        fn () => AirtimeTransaction::make(
-            phoneNumber: '+256783879001',
-            currency: Currency::UGANDA,
-            amount: faker()->numberBetween(100, 300)
-        ),
-        fn () => AirtimeTransaction::make(
-            phoneNumber: '+260977528773',
-            currency: Currency::ZAMBIA,
-            amount: faker()->numberBetween(100, 300)
-        ),
-        fn () => AirtimeTransaction::make(
-            phoneNumber: '+27714225174',
-            currency: Currency::SOUTH_AFRICA,
-            amount: faker()->numberBetween(100, 300)
-        ),
-        fn () => AirtimeTransaction::make(
-            phoneNumber: '+2348160663047',
-            currency: Currency::NIGERIA,
-            amount: faker()->numberBetween(100, 300)
-        ),
-    ],
-]);
+dataset('airtime-transactions', function () {
+    yield fn() => AirtimeTransaction::make(
+        phoneNumber: '+254700072929',
+        currency: Currency::KENYA,
+        amount: faker()->numberBetween(100, 300)
+    );
+
+    yield fn() => AirtimeTransaction::make(
+        phoneNumber: '+256783879001',
+        currency: Currency::UGANDA,
+        amount: faker()->numberBetween(100, 300)
+    );
+
+    yield fn() => AirtimeTransaction::make(
+        phoneNumber: '+255757022731',
+        currency: Currency::TANZANIA,
+        amount: faker()->numberBetween(1000, 3000)
+    );
+
+    yield fn() => AirtimeTransaction::make(
+        phoneNumber: '+2348160663047',
+        currency: Currency::NIGERIA,
+        amount: faker()->numberBetween(100, 300)
+    );
+});
 
 dataset('airtime-amount', [
-    fn () => faker()->numberBetween(100, 150),
+    fn() => faker()->numberBetween(100, 150),
 ]);
