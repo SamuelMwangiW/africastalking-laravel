14e877b2428788d0461e678ece43e968fa1ac8dd
SamuelMwangiW <SamuelMwangiW@users.noreply.github.com>
2022-04-28 01:26:48 +0000

    Fix styling

--- tests/MobileCheckoutTest.php
+++ tests/MobileCheckoutTest.php
@@ -3,10 +3,10 @@
 use SamuelMwangiW\Africastalking\Domain\MobileCheckout;
 
 it('resolves the class')
-    ->expect(fn() => app(MobileCheckout::class))->toBeInstanceOf(MobileCheckout::class);
+    ->expect(fn () => app(MobileCheckout::class))->toBeInstanceOf(MobileCheckout::class);
 
 it('resolves the class via the helper')
-    ->expect(fn() => africastalking()->payment()->mobileCheckout())->toBeInstanceOf(MobileCheckout::class);
+    ->expect(fn () => africastalking()->payment()->mobileCheckout())->toBeInstanceOf(MobileCheckout::class);
 
 it('sends a Mobile Checkout Request', function (string $phone, int $amount) {
     $result = africastalking()
