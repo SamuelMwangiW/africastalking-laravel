2ae2030ed185ecd885ce7eb8e7d04e3266ca2b16
Samuel Mwangi <samuel@samuelmwangi.co.ke>
2022-04-28 03:51:42 +0300

    Extract Payment Base Request

--- src/Transporter/Requests/Payment/StashTopupRequest.php
+++ src/Transporter/Requests/Payment/StashTopupRequest.php
@@ -2,12 +2,7 @@
 
 namespace SamuelMwangiW\Africastalking\Transporter\Requests\Payment;
 
-use SamuelMwangiW\Africastalking\Transporter\Requests\AfricastalkingRequest;
-
-class StashTopupRequest extends AfricastalkingRequest
+class StashTopupRequest extends PaymentRequest
 {
     protected string $path = 'topup/stash';
-
-    protected string $liveBaseUrl = 'https://payments.africastalking.com/';
-    protected string $sandboxBaseUrl = 'https://payments.sandbox.africastalking.com/';
 }
--- src/Transporter/Requests/Payment/WalletBalanceRequest.php
+++ src/Transporter/Requests/Payment/WalletBalanceRequest.php
@@ -2,13 +2,8 @@
 
 namespace SamuelMwangiW\Africastalking\Transporter\Requests\Payment;
 
-use SamuelMwangiW\Africastalking\Transporter\Requests\AfricastalkingRequest;
-
-class WalletBalanceRequest extends AfricastalkingRequest
+class WalletBalanceRequest extends PaymentRequest
 {
     protected string $method = 'GET';
     protected string $path = 'query/wallet/balance';
-
-    protected string $liveBaseUrl = 'https://payments.africastalking.com/';
-    protected string $sandboxBaseUrl = 'https://payments.sandbox.africastalking.com/';
 }
