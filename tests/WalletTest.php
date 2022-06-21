b83bd23d75f82de8040523ba400f102e7bbd5fea
Samuel Mwangi <samuel@samuelmwangi.co.ke>
2022-06-21 22:23:00 +0300

    formatting

--- tests/WalletTest.php
+++ tests/WalletTest.php
@@ -4,13 +4,21 @@ use SamuelMwangiW\Africastalking\Domain\Wallet;
 use SamuelMwangiW\Africastalking\ValueObjects\Balance;
 
 it('can be resolved')
-    ->expect(fn () => app(Wallet::class))->toBeInstanceOf(Wallet::class);
+    ->expect(
+        fn() => app(Wallet::class)
+    )->toBeInstanceOf(Wallet::class);
 
 it('can be resolved via helper')
-    ->expect(fn () => africastalking()->wallet())->toBeInstanceOf(Wallet::class);
+    ->expect(
+        fn() => africastalking()->wallet()
+    )->toBeInstanceOf(Wallet::class);
 
 it('can fetch balance')
-    ->expect(fn () => app(Wallet::class)->balance())->toBeInstanceOf(Balance::class);
+    ->expect(
+        fn() => app(Wallet::class)->balance()
+    )->toBeInstanceOf(Balance::class);
 
 it('can fetch balance via helper')
-    ->expect(fn () => africastalking()->wallet()->balance())->toBeInstanceOf(Balance::class);
+    ->expect(
+        fn() => africastalking()->wallet()->balance()
+    )->toBeInstanceOf(Balance::class);
