372c438c175a53084d84246e3261860050f674c7
Samuel Mwangi <samuel@samuelmwangi.co.ke>
2022-09-17 10:25:33 +0300

    added Invalid callerId:

--- tests/VoiceTest.php
+++ tests/VoiceTest.php
@@ -83,5 +83,5 @@ it('makes a call', function () {
     expect($response)
         ->toBeArray()
         ->toHaveKeys(['entries', 'errorMessage'])
-        ->and($response['errorMessage'])->toBeIn(['None','Invalid callbackUrl: ']);
+        ->and($response['errorMessage'])->toBeIn(['None','Invalid callbackUrl: ','Invalid callerId: ']);
 });
