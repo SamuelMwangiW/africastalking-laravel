10e80c04c67d250dd6b4b56d1abd00dac779afbc
Samuel Mwangi <samuel@samuelmwangi.co.ke>
2023-01-23 11:28:11 +0300

    Airtime Dataset

--- tests/Datasets/airtime.php
+++ tests/Datasets/airtime.php
@@ -7,31 +7,31 @@ use SamuelMwangiW\Africastalking\ValueObjects\AirtimeTransaction;
 
 use function Pest\Faker\faker;
 
-dataset('airtime-transactions', function () {
-    yield fn () => AirtimeTransaction::make(
+dataset('airtime-transactions', [
+    'KENYA airtime transaction to +254700072929' => fn () => AirtimeTransaction::make(
         phoneNumber: '+254700072929',
         currency: Currency::KENYA,
         amount: faker()->numberBetween(10, 300)
-    );
+    ),
 
-    yield fn () => AirtimeTransaction::make(
+    'UGANDA airtime transaction to +256783879001' => fn () => AirtimeTransaction::make(
         phoneNumber: '+256783879001',
         currency: Currency::UGANDA,
         amount: faker()->numberBetween(100, 1_000)
-    );
+    ),
 
-    yield fn () => AirtimeTransaction::make(
+    'TANZANIA airtime transaction to +255757022731' => fn () => AirtimeTransaction::make(
         phoneNumber: '+255757022731',
         currency: Currency::TANZANIA,
         amount: faker()->numberBetween(1000, 3000)
-    );
+    ),
 
-    yield fn () => AirtimeTransaction::make(
+    'NIGERIA airtime transaction to +2348160663047' => fn () => AirtimeTransaction::make(
         phoneNumber: '+2348160663047',
         currency: Currency::NIGERIA,
         amount: faker()->numberBetween(100, 1_000)
-    );
-});
+    ),
+]);
 
 dataset('airtime-amount', [
     fn () => faker()->numberBetween(10, 500),
