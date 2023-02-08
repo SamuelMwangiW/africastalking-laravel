3a451edfa9765f5f2752cbd28768216c80e7a2da
Samuel Mwangi <samuel@samuelmwangi.co.ke>
2023-02-08 11:23:37 +0300

    Move from config to an sms group

--- config/africastalking.php
+++ config/africastalking.php
@@ -5,7 +5,9 @@ declare(strict_types=1);
 return [
     'username' => env('AFRICASTALKING_USERNAME', 'sandbox'),
     'api-key' => env('AFRICASTALKING_API_KEY'),
-    'from' => env('AFRICASTALKING_FROM'), //to extract this to an sms group
+    'sms' => [
+        'from' => env('AFRICASTALKING_FROM'),
+    ],
     'payment' => [
         'product-name' => env('AFRICASTALKING_PAYMENT_PRODUCT'),
     ],
