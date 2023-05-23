053a232f20c69f0961354f5da6eec888549662a8
Samuel Mwangi <samuel@samuelmwangi.co.ke>
2023-05-23 14:27:14 +0300

    Add tests

---
+++ tests/Unit/ValueObjects/StashTopupResponseTest.php
@@ -0,0 +1,58 @@
+<?php
+
+declare(strict_types=1);
+
+use SamuelMwangiW\Africastalking\Contracts\DTOContract;
+use SamuelMwangiW\Africastalking\Enum\Status;
+use SamuelMwangiW\Africastalking\ValueObjects\StashTopupResponse;
+
+it('can be constructed')
+    ->expect(fn () => new StashTopupResponse(
+        id: 'id',
+        status: Status::SENT,
+        description: 'description'
+    ))
+    ->toBeInstanceOf(StashTopupResponse::class)
+    ->id->toBe('id')
+    ->description->toBe('description');
+
+it('can be constructed statically')
+    ->expect(fn () => StashTopupResponse::make(
+        [
+            'transactionId' => 'id',
+            'status' => Status::SENT->value,
+            'description' => 'description',
+        ]
+    ))
+    ->toBeInstanceOf(StashTopupResponse::class)
+    ->id->toBe('id')
+    ->description->toBe('description');
+
+it('implements DTO contract')
+    ->expect(fn () => new StashTopupResponse(
+        id: 'id',
+        status: Status::SENT,
+        description: 'description'
+    ))
+    ->toBeInstanceOf(DTOContract::class);
+
+it('can be cast to array')
+    ->expect(fn () => new StashTopupResponse(
+        id: 'id',
+        status: Status::SENT,
+        description: 'description'
+    ))
+    ->__toArray()->toBeArray()->toMatchArray(['id'=>'id','description'=>'description']);
+
+it('can be cast to string', function (): void {
+    $object = new StashTopupResponse(
+        id: 'id',
+        status: Status::SENT,
+        description: 'description'
+    );
+
+    expect((string)$object)
+        ->toBeString()
+        ->toBe($object->__toString())
+        ->toBe('{"id":"id","status":"Sent","description":"description"}');
+});
