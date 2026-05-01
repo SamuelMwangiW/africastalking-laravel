7f1568b918afa69566095091ea7adc4c6291b917
Samuel Mwangi <samuel@samuelmwangi.co.ke>
2026-05-01 11:23:44 +0300

    Added Laravel boost guideline

---
+++ resources/boost/guidelines/core.blade.php
@@ -0,0 +1,11 @@
+@php
+    /** @var \Laravel\Boost\Install\GuidelineAssist $assist */
+@endphp
+
+## Africa's Talking SDK
+
+- Laravel SDK for sending SMS messages, airtime disbursement and voice calls.
+- SDK Documentation: https://at-laravel-docs.pages.dev
+- Official Africa's Talking api Documentation: https://developers.africastalking.com
+- Use `web-search` tool for latest docs before implementing.
+- IMPORTANT: Activate africastalking-laravel skill when working with SMS, USSD, Voice, Airtime and Africa's Talking api related tasks.
