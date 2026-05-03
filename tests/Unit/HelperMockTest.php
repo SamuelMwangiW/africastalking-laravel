6964e6c1bfb10a91fe30cec34d3f119e84c165c6
Samuel Mwangi <samuel@samuelmwangi.co.ke>
2026-05-03 16:00:01 +0300

    Fix return type

--- tests/Unit/HelperMockTest.php
+++ tests/Unit/HelperMockTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace SamuelMwangiW\Africastalking\Tests\Unit;
 
+use SamuelMwangiW\Africastalking\Africastalking as BaseClass;
 use SamuelMwangiW\Africastalking\Facades\Africastalking;
 use SamuelMwangiW\Africastalking\Tests\TestCase;
 
@@ -24,20 +25,23 @@ class HelperMockTest extends TestCase
 
 class GlobalFunctionsMockPlaceholder
 {
-    public function africastalking(): void {}
+    public function africastalking(): BaseClass
+    {
+        return new BaseClass();
+    }
 }
 
 class GlobalFunctionsMocker
 {
     public static GlobalFunctionsMockPlaceholder $mock;
 
-    public static function africastalking()
+    public static function africastalking(): BaseClass
     {
         return call_user_func([self::$mock, 'africastalking']);
     }
 }
 
-function africastalking()
+function africastalking(): BaseClass
 {
     return call_user_func([GlobalFunctionsMocker::class, 'africastalking']);
 }
