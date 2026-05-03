6007458d7ed70061cc88995bba7c66a06336e342
Samuel Mwangi <samuel@samuelmwangi.co.ke>
2026-05-03 15:57:12 +0300

    Test mocking global functions

---
+++ tests/Unit/HelperMockTest.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SamuelMwangiW\Africastalking\Tests\Unit;
+
+use SamuelMwangiW\Africastalking\Facades\Africastalking;
+use SamuelMwangiW\Africastalking\Tests\TestCase;
+
+class HelperMockTest extends TestCase
+{
+    public function testAfricastalkingGlobalFunction(): void
+    {
+        $baseClass = Africastalking::getFacadeRoot();
+        $GlobalFunctionMock = $this->createMock(GlobalFunctionsMockPlaceholder::class);
+        $GlobalFunctionMock->expects($this->once())
+            ->method('africastalking')
+            ->willReturn($baseClass);
+        GlobalFunctionsMocker::$mock = $GlobalFunctionMock;
+
+        $this->assertSame($baseClass, africastalking());
+    }
+}
+
+class GlobalFunctionsMockPlaceholder
+{
+    public function africastalking(): void {}
+}
+
+class GlobalFunctionsMocker
+{
+    public static GlobalFunctionsMockPlaceholder $mock;
+
+    public static function africastalking()
+    {
+        return call_user_func([self::$mock, 'africastalking']);
+    }
+}
+
+function africastalking()
+{
+    return call_user_func([GlobalFunctionsMocker::class, 'africastalking']);
+}
