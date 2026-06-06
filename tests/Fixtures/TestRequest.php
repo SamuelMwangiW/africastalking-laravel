f7566964c626c2c829fcbf9c15b66ba3b8b5c1c7
Samuel Mwangi <samuel@samuelmwangi.co.ke>
2026-06-06 18:10:41 +0300

    Make phoneNumber property on Capability token required

--- tests/Fixtures/TestRequest.php
+++ tests/Fixtures/TestRequest.php
@@ -15,6 +15,10 @@ class TestRequest extends Request implements HasBody
 {
     use HasJsonBody;
 
+    public function __construct(public string $phoneNumber = '')
+    {
+    }
+
     protected Method $method = Method::POST;
 
     public function __construct(public string $phoneNumber = '') {}
